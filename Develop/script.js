bdd385e34a3cf5862619452829e129a1e6588d72
troat9824 <troat9824@gmail.com>
2022-02-09 04:28:31 -0600

    start of prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,46 @@
 // Assignment code here
+var character = {
+  lowercase: "",
+  uppercase: "",
+  num: "",
+  specCharacter: "",
+};
+
+function generatePassword() {
+  var promptLength = window.prompt("Please choose a password length between 8 and 128 characters.");
+
+  var promptLower = window.confirm('Would you like your password to contain Lowercase letters?');
+    if(promptLower === true) {
+      window.alert("Your password will include Lowercase Letters.");
+    }
+    if (promptLower === false) {
+      window.alert("Your password will not include Lowercase Letters.");
+    };
+  
+  var promptUpper = window.confirm("Would you like your password to contain Uppercase letters?");
+    if(promptUpper === true) {
+      window.alert("Your password will include Uppercase letters.");
+    };
+    if (promptUpper === false) {
+      window.alert("Your password will not include Uppercase letters.");
+    };
+  
+  var promptNum = window.confirm("Would you like your password to contain numbers?");
+    if(promptNum === true) {
+      window.alert("Your password will include numbers.");
+    };
+    if(promptNum === false) {
+      window.alert("Your password will not include numbers.");
+    };
+
+  var promptSpec = window.confirm("Would you like your password to contain special characters?");
+    if(promptSpec === true) {
+      window.alert("Your password will include special characters.");
+    };
+    if(promptSpec === false) {
+      window.alert("Your password will not include special characters.");
+    };
+};
 
 
 // Get references to the #generate element
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Password Generator Starter Code
+# Let's Make A Password
+
+## Purpose
+* A password generator for your online security needs.
+
+## Built With
+* JavaScript
+* HTML
+* CSS
+
+## Website
+https://troat9824.github.io/Lets-Make-A-Password/
+
+# Contribution
+Made by Taylor Vaughn
+
