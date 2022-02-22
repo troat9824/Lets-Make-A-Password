e689690312f18c141a5b77e7d44097769fc047ac
troat9824 <troat9824@gmail.com>
2022-02-21 19:20:29 -0600

    ifTrue update

--- Develop/script.js
+++ Develop/script.js
@@ -3,17 +3,20 @@ var character = {
   lowercase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
   uppercase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
   num: ["0","1","2","3","4","5","6","7","8","9"],
-  specCharacter: ["!","@","#","$","%","^","&*"],
+  specCharacter: ["!","@","#","$","%","^","&","*"],
 };
-var pwd ="";
 
-var ifTrue = [];
 
-randomizer(character.lowercase);
+
+
+
 
 // var ifTrue = [true, false, true, true]
 
 function generatePassword() {
+  var pwd = "";
+  var ifTrue = [];
+  var passwordLength ="";
   var promptLength = window.prompt("Please choose a password length between 8 and 128 characters.");
     if(promptLength < 8 || promptLength > 128) {
       window.alert("Please choose a number between 8 and 128.");
@@ -22,12 +25,13 @@ function generatePassword() {
 
     if(promptLength >= 8 && promptLength <= 128) {
       window.alert("Your password will be " + promptLength + " characters long.")
+      passwordLength = promptLength;
     };
     
   var promptLower = window.confirm('Would you like your password to contain Lowercase letters?');
     if(promptLower === true) {
       window.alert("Your password will include Lowercase Letters.");
-      ifTrue.push(promptLower);
+      ifTrue = ifTrue.concat(character.lowercase);
     } else {
       window.alert("Your password will not include Lowercase Letters.");
     };
@@ -35,7 +39,7 @@ function generatePassword() {
   var promptUpper = window.confirm("Would you like your password to contain Uppercase letters?");
     if(promptUpper === true) {
       window.alert("Your password will include Uppercase letters.");
-      ifTrue.push(promptUpper);
+      ifTrue = ifTrue.concat(character.uppercase);
     } else {
       window.alert("Your password will not include Uppercase letters.");
     };
@@ -43,7 +47,7 @@ function generatePassword() {
   var promptNum = window.confirm("Would you like your password to contain numbers?");
     if(promptNum === true) {
       window.alert("Your password will include numbers.");
-      ifTrue.push(promptNum);
+      ifTrue = ifTrue.concat(character.num);
     } else {
       window.alert("Your password will not include numbers.");
     };
@@ -51,55 +55,26 @@ function generatePassword() {
   var promptSpec = window.confirm("Would you like your password to contain special characters?");
     if(promptSpec === true) {
       window.alert("Your password will include special characters.");
-      ifTrue.push(promptSpec);
+      ifTrue = ifTrue.concat(character.specCharacter);
     } else {
       window.alert("Your password will not include special characters.");
     };
 console.log(ifTrue);
 
 
-for (let i=0; pwd.length < promptLength.length; i++) {
-
+for (var i = 0; i < passwordLength; i++) {
        
-  if (promptLower) {
-    console.log(`lowercase yes`);
-    let randomizerLowercase =
-      character.lowercase[Math.floor(Math.random() * character.lowercase.length)];
-    console.log(`randomizerLowercase: ${randomizerLowercase}`);
-    pwd = pwd + randomizerLowercase;
-  }
+pwd += ifTrue[Math.floor(Math.random() * ifTrue.length)];
+  
+};
 
-  if (promptUpper) {
-    console.log(`uppercase yes`);
-    let randomizerUppercase =
-      character.uppercase[Math.floor(Math.random() * character.uppercase.length)];
-    console.log(`randomizerUppercase: ${randomizerUppercase}`);
-    pwd = pwd + randomizerUppercase;
-  }
 
- 
-  if (promptNum) {
-    console.log(`num yes`);
-    let randomizerNum =
-      character.num[Math.floor(Math.random() * character.num.length)];
-    console.log(`randomizerNum: ${randomizerNum}`);
-    pwd = pwd + randomizerNum;
-  }
 
- 
-  if (promptSpec) {
-    console.log(`spec yes`);
-    let randomizerSpec =
-      character.specCharacter[Math.floor(Math.random() * character.specCharacter.length)];
-    console.log(`randomizerSpec: ${randomizerSpec}`);
-    pwd = pwd + randomizerSpec;
-  }
 
 // Return the final password generated
 console.log(`pwd: ${pwd}`);
 console.log(`pwd length: ${pwd.length}`);
 return pwd;
-};
 };
 
 // Get references to the #generate element
